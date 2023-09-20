01c87919d6f237bb55ca485420a696ca22679667
Horacio Mota <horaciomota93@gmail.com>
2023-09-20 19:11:22 +0100

    First UI added

--- GithubApp/ContentView.swift
+++ GithubApp/ContentView.swift
@@ -9,13 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        HStack {
+            Rectangle()
+                .frame(width: 105, height: 120)
+                .cornerRadius(12)
+            
+            VStack (alignment: .leading) {
+                
+                
+                Text("Horacio Mota")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.primary)
+                
+                Text("Mobile developer  -  Swift")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .foregroundColor(.secondary)
+                    .padding(.bottom)
+                
+                HStack (spacing: 12) {
+                    
+                    VStack {
+                        Text("50")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                        
+                        Text("Repositories")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    VStack {
+                        Text("10k")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                        
+                        Text("Followers")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    VStack {
+                        Text("20k")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                        
+                        Text("Following")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    
+                }
+                .cornerRadius(12)
+
+            }
+            
+            
+        }.padding()
+    
     }
 }
 

d569b269be88cd10f45c92d08563e228b6ba6679
Horacio Mota <horaciomota93@gmail.com>
2023-09-20 17:10:40 +0100

    First Commit

---
+++ GithubApp/GithubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAppApp.swift
+//  GithubApp
+//
+//  Created by Horacio Mota on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

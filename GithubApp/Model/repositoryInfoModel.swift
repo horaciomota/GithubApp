3f4c46f1a5550cab4b1ec842ba41aaa75fff5ed3
Horacio Mota <horaciomota93@gmail.com>
2023-09-21 21:18:23 +0100

    repositoryModel added, mocking dat

---
+++ GithubApp/Model/repositoryInfoModel.swift
@@ -0,0 +1,16 @@
+//
+//  repositoryInfoModel.swift
+//  GithubApp
+//
+//  Created by Horacio Mota on 21/09/2023.
+//
+
+import Foundation
+
+struct repositoryInfo: Decodable, Identifiable {
+    
+    let id: Int
+    let name: String
+    let description: String
+    
+}
--- GithubApp/Networking/repositoryInfoData.swift
+++ GithubApp/Networking/repositoryInfoData.swift
@@ -5,11 +5,11 @@
 //  Created by Horacio Mota on 21/09/2023.
 //
 
-import import Foundation
+import Foundation
 
-struct repositoryInfoData: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+func repositoryInfoData() async throws -> [repositoryInfo] {
+    
+    
+    
+    
 }
-
